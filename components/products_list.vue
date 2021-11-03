f963a041b9cb8aab41ea154ba453bf3c45e4dc8f
stas <kyaune@gmail.com>
2021-11-04 00:15:12 +0300

    made product card component, added icon (wip)

--- components/products_list.vue
+++ components/products_list.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-  <Product></Product>
+  <div class="main">
+    <div class="container">
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+    </div>
   </div>
 </template>
 <script>
-import Product from '@/components/product'
+import ProductCard from '@/components/product_card'
 export default {
   components: {
-    Product
+    ProductCard
   }
 }
 </script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  /*justify-content: space-between;*/
+  gap: 16px 16px;
+}
+</style>
