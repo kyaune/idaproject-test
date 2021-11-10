ae07165937f0cb627de547d1560168ac73c0bad6
stas <kyaune@gmail.com>
2021-11-10 04:42:20 +0300

    added build files

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/idaproject-test'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'idaproject-test',
